5d6dd068a65eb5dca874e409673896f86a0f89d6
A John Peters <ajpeters@outlook.com>
2018-04-19 09:25:31 -0500

    Add project files.

---
+++ listView Demo/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace listView_Demo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.demolistView = new System.Windows.Forms.ListView();
+            this.listviewhdrLabel = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.printButton = new System.Windows.Forms.Button();
+            this.quitButton = new System.Windows.Forms.Button();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // demolistView
+            // 
+            this.demolistView.GridLines = true;
+            this.demolistView.Location = new System.Drawing.Point(232, 87);
+            this.demolistView.MultiSelect = false;
+            this.demolistView.Name = "demolistView";
+            this.demolistView.Size = new System.Drawing.Size(238, 184);
+            this.demolistView.TabIndex = 0;
+            this.demolistView.UseCompatibleStateImageBehavior = false;
+            // 
+            // listviewhdrLabel
+            // 
+            this.listviewhdrLabel.AutoSize = true;
+            this.listviewhdrLabel.Location = new System.Drawing.Point(295, 61);
+            this.listviewhdrLabel.Name = "listviewhdrLabel";
+            this.listviewhdrLabel.Size = new System.Drawing.Size(77, 13);
+            this.listviewhdrLabel.TabIndex = 1;
+            this.listviewhdrLabel.Text = "ListView Demo";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(495, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "display files";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // printButton
+            // 
+            this.printButton.Location = new System.Drawing.Point(36, 41);
+            this.printButton.Name = "printButton";
+            this.printButton.Size = new System.Drawing.Size(75, 53);
+            this.printButton.TabIndex = 3;
+            this.printButton.Text = "Print Selection";
+            this.printButton.UseVisualStyleBackColor = true;
+            this.printButton.Click += new System.EventHandler(this.printButton_Click);
+            // 
+            // quitButton
+            // 
+            this.quitButton.Location = new System.Drawing.Point(36, 100);
+            this.quitButton.Name = "quitButton";
+            this.quitButton.Size = new System.Drawing.Size(75, 23);
+            this.quitButton.TabIndex = 4;
+            this.quitButton.Text = "Quit";
+            this.quitButton.UseVisualStyleBackColor = true;
+            this.quitButton.Click += new System.EventHandler(this.quitButton_Click);
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.AutoSize = true;
+            this.outputLabel.Location = new System.Drawing.Point(36, 167);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(39, 13);
+            this.outputLabel.TabIndex = 5;
+            this.outputLabel.Text = "Output";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.quitButton);
+            this.Controls.Add(this.printButton);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listviewhdrLabel);
+            this.Controls.Add(this.demolistView);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView demolistView;
+        private System.Windows.Forms.Label listviewhdrLabel;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button printButton;
+        private System.Windows.Forms.Button quitButton;
+        private System.Windows.Forms.Label outputLabel;
+    }
+}
+
---
+++ listView Demo/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace listView_Demo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           // demolistView.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent);
+           // demolistView.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize);
+            demolistView.View = View.Details;
+             demolistView.Width =400;
+            List<string> name = new List<string> { "Joe", "Jill", "aaron","Steven" };
+            List<string> phone = new List<string> { "555-555-5555", "666-666-6666", "777-777-7777","888-888-888" };
+            List<string> email = new List<string> { "Joe@a.com", "Jill@b.com", "aaron@c.com","Steven@d.com" };
+            List<string> program = new List<string> { "Programming", "Networking", "Specialist","astro physisi" };
+            for (int i=0;i<name.Count(); i++)
+            {
+                ListViewItem item = new ListViewItem(name[i]);
+                item.SubItems.Add(phone[i]);
+                item.SubItems.Add(email[i]);
+                item.SubItems.Add(program[i]);
+                demolistView.Items.Add(item); 
+            }
+           /* ListViewItem item1 = new ListViewItem("Joe");
+
+             item1.SubItems.Add("555-555-5678");
+             item1.SubItems.Add("a@b.com");
+             item1.SubItems.Add("programming");
+
+             ListViewItem item2 = new ListViewItem("Jill");
+             item2.SubItems.Add("555-678-2345");
+             item2.SubItems.Add("c@d.com");
+             item2.SubItems.Add("networking");
+
+             ListViewItem item3 = new ListViewItem("Aaron");
+             item3.SubItems.Add("555-543-6666");
+             item3.SubItems.Add("e@f.com");
+             item3.SubItems.Add("other");*/
+             // Create columns for the items and subitems.
+             // Width of -2 indicates auto-size.
+             demolistView.Columns.Add("Name           ", -2, HorizontalAlignment.Left);
+             demolistView.Columns.Add("Phone   ", -2, HorizontalAlignment.Left);
+             demolistView.Columns.Add("email            ", -2, HorizontalAlignment.Left);
+             demolistView.Columns.Add("Program     ", -2, HorizontalAlignment.Left);
+
+             
+
+            demolistView.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            demolistView.Width = 270;
+            demolistView.Location = new System.Drawing.Point(250, 10);
+
+            // Declare and construct the ColumnHeader objects.
+            ColumnHeader header1, header2;
+            header1 = new ColumnHeader();
+            header2 = new ColumnHeader();
+
+            // Set the text, alignment and width for each column header.
+            header1.Text = "File name";
+            header1.TextAlign = HorizontalAlignment.Left;
+            header1.Width = 70;
+
+            header2.TextAlign = HorizontalAlignment.Left;
+            header2.Text = "Location";
+            header2.Width = 200;
+
+            // Add the headers to the ListView control.
+            demolistView.Columns.Add(header1);
+            demolistView.Columns.Add(header2);
+
+            // Specify that each item appears on a separate line.
+            demolistView.View = View.Details;
+
+            // Populate the ListView.Items property.
+            // Set the directory to the sample picture directory.
+            System.IO.DirectoryInfo dirInfo =
+                new System.IO.DirectoryInfo(
+                "C:\\Documents and Settings\\ajpeters" +
+                "\\Pictures");
+
+
+            // Get the .jpg files from the directory
+            System.IO.FileInfo[] files = dirInfo.GetFiles("*.jpg");
+
+            // Add each file name and full name including path
+            // to the ListView.
+            if (files != null)
+            {
+                foreach (System.IO.FileInfo file in files)
+                {
+                    ListViewItem item = new ListViewItem(file.Name);
+                    item.SubItems.Add(file.FullName);
+                    demolistView.Items.Add(item);
+                }
+            }
+        }
+
+        private void quitButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void printButton_Click(object sender, EventArgs e)
+        {
+            if (demolistView.SelectedItems.Count > 0)
+            {
+                ListViewItem item = demolistView.SelectedItems[0];
+                outputLabel.Text = item.SubItems[0].Text;
+                outputLabel.Text += "\n"+item.SubItems[1].Text;
+                outputLabel.Text += "\n" + item.SubItems[2].Text;
+                outputLabel.Text += "\n" + item.SubItems[3].Text;
+            }
+            else
+            {
+               outputLabel.Text = string.Empty;
+                
+            }
+        }
+    }
+    }
+
